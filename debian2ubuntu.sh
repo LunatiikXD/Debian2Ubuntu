d6414a536bfd0dd10eeb748097a47784c5091a35
LunatiikXD <36826729+LunatiikXD@users.noreply.github.com>
2020-02-26 16:50:21 -0500

    Update debian2ubuntu.sh

--- debian2ubuntu.sh
+++ debian2ubuntu.sh
@@ -8,7 +8,7 @@ sudo apt-get install curl -y
 curl https://raw.githubusercontent.com/LunatiikXD/weirdscripts/master/ubuntusources.txt | sudo tee /etc/apt/sources.list
 sudo apt update
 sudo apt-get dist-upgrade -y
-sudo apt --fix-broken install
+sudo apt --fix-broken install -y
 sudo apt-get purge ghostscript libgs9 libgs9-common python3-cupshelpers -y
 sudo systemctl restart networking.service
 sudo apt-get install ubuntu-desktop -y
