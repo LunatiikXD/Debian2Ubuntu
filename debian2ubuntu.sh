1d95db01c65401d2d409577f9eeff1ecdb5bda3c
LunatiikXD <36826729+LunatiikXD@users.noreply.github.com>
2020-02-26 16:40:52 -0500

    Update debian2ubuntu.sh

--- debian2ubuntu.sh
+++ debian2ubuntu.sh
@@ -4,14 +4,15 @@ echo "Welcome to the Debian2Ubuntu,"
 echo "Debian will be turned into Ubuntu,"
 echo "you can grab a cup of coffee and wait."
 echo -e "\e[1m================================================="
-sudo apt-get install curl
+sudo apt-get install curl -y
 curl https://raw.githubusercontent.com/LunatiikXD/weirdscripts/master/ubuntusources.txt | sudo tee /etc/apt/sources.list
 sudo apt update
-sudo apt-get dist-upgrade
+sudo apt-get dist-upgrade -y
 sudo apt --fix-broken install
-sudo apt-get purge ghostscript libgs9 libgs9-common python3-cupshelpers
+sudo apt-get purge ghostscript libgs9 libgs9-common python3-cupshelpers -y
 sudo systemctl restart networking.service
-sudo apt-get install ubuntu-desktop
+sudo apt-get install ubuntu-desktop -y
+sudo reboot
 wget -N https://raw.githubusercontent.com/CollabVM-Retards/dotfiles/master/.bashrc
 exec bash
 echo "Enjoy your VM"
