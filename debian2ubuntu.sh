2baa5afe1b980a9e19cab5ce959ffe68d6b59e3c
LunatiikXD <36826729+LunatiikXD@users.noreply.github.com>
2020-02-26 16:12:21 -0500

    Update debian2ubuntu.sh

--- debian2ubuntu.sh
+++ debian2ubuntu.sh
@@ -4,7 +4,7 @@ echo "Welcome to the Debian2Ubuntu,"
 echo "Debian will be turned into Ubuntu,"
 echo "you can grab a cup of coffee and wait."
 echo -e "\e[1m================================================="
-sudo apt-get install tee
+sudo apt-get install curl
 curl https://raw.githubusercontent.com/LunatiikXD/weirdscripts/master/ubuntusources.txt | sudo tee /etc/apt/sources.list
 sudo apt update
 sudo apt-get dist-upgrade
