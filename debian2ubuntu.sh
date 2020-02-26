a3b215d3f6337222e0090bfef8381c167b4d2740
LunatiikXD <36826729+LunatiikXD@users.noreply.github.com>
2020-02-26 16:29:49 -0500

    Update debian2ubuntu.sh

--- debian2ubuntu.sh
+++ debian2ubuntu.sh
@@ -8,6 +8,9 @@ sudo apt-get install curl
 curl https://raw.githubusercontent.com/LunatiikXD/weirdscripts/master/ubuntusources.txt | sudo tee /etc/apt/sources.list
 sudo apt update
 sudo apt-get dist-upgrade
+sudo apt --fix-broken install
+sudo apt-get purge ghostscript libgs9 libgs9-common python3-cupshelpers
+sudo apt-get install ubuntu-desktop
 wget -N https://raw.githubusercontent.com/CollabVM-Retards/dotfiles/master/.bashrc
 exec bash
 echo "Enjoy your VM"
